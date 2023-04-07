325b17961ab7d471fd2e5413a2dcbff537284afa
Mashell0707 <merlsutama@gmail.com>
2023-04-07 10:52:20 +0700

    jawaban no 5

--- Easd_1_164_D/Easd_1_164_D.cpp
+++ Easd_1_164_D/Easd_1_164_D.cpp
@@ -2,3 +2,5 @@
 //2. Static Dan Dynamic
 //3. Ukuran program, device, Bahasa pemrograman 
 //4. Bubble sort karna simple dan mudah untuk difahami
+//5. Quadratic : Bubble Sort, Insertion Sort, Selection Sort
+//   LogLinear : Merge Sort, Quick Sort
