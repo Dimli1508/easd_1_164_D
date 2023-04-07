bd4b22cd343a8e5cb7f548fcd6a5a9fb2a4b0140
Mashell0707 <merlsutama@gmail.com>
2023-04-07 10:59:05 +0700

    Commit hasil akhir semuanya, tadi sudah bikin dari awal kehapus jadi takut kurang waktunya cuma ada sedikit commitnya

--- Easd_1_164_D/Easd_1_164_D.cpp
+++ Easd_1_164_D/Easd_1_164_D.cpp
@@ -7,3 +7,66 @@
 
 #include <iostream>
 using namespace std;
+
+int dimas[30];
+int n;
+
+void selectionsort() {
+    for (int j = 0; j < n - 1; j++) {
+        int min_index = j;
+        for (int i = j + 1; i < n; i++)
+        {
+            if (dimas[i] < dimas[min_index])
+            {
+                min_index = i;
+            }
+        }
+        if (min_index != j)
+        {
+            int temp = dimas[j];
+            dimas[j] = dimas[min_index];
+            dimas[min_index] = temp;
+        }
+    }
+}
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan banyaknya elemen pada array : ";
+        cin >> n; 
+        if (n <= 30) 
+            break;
+        else
+        { 
+            cout << "\nArray dapat mempunyai maksimal 30 elemen.";  
+        }
+    }
+    cout << endl;
+    cout << "Masukkan Elemen Array" << endl;   
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data Ke-" << (i + 1) << ": ";
+        cin >> dimas[i];
+    }
+}
+
+
+
+void display() {
+    cout << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    for (int j = 0; j < n; j++) { 
+        cout << dimas[j] << endl;
+    }
+    cout << endl; 
+}
+
+
+int main()
+{
+    input();
+    selectionsort();
+    display();
+}
