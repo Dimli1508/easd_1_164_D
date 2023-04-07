7d6618fd53a26f9d226594555fd9bf3d58a78e98
Mashell0707 <merlsutama@gmail.com>
2023-04-07 10:51:37 +0700

    jawaban no 3

--- Easd_1_164_D/Easd_1_164_D.cpp
+++ Easd_1_164_D/Easd_1_164_D.cpp
@@ -1,2 +1,3 @@
 //1. Algoritma dibutuhkan supaya suatu masalah bisa diselesaikan secara lebih mudah karena sudah tertata dan urut
 //2. Static Dan Dynamic
+//3. Ukuran program, device, Bahasa pemrograman 
